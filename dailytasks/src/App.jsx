4d8e8f637558b382906e24415d7a8740b30bcc69
Valeriusdev <kakao757@gmail.com>
2024-02-10 19:14:24 -0800

    update App.jsx file

--- dailytasks/src/App.jsx
+++ dailytasks/src/App.jsx
@@ -1,12 +1,9 @@
 import React from 'react';
-import AllTasks from './components/AllTasks';
-import Calander from './components/Calander';
 import Sidebar from './components/SideBar';
 
 const App = () => {
   return (
     <>
-      <Calander />
       <Sidebar />    
     </>
   );
