1751315a8f77e96ecaaca0a6006e08bf0eaf4722
Alex Singh <115970252+singhalex@users.noreply.github.com>
2024-02-14 11:38:45 -0800

    No Router component yet

--- dailytasks/src/main.jsx
+++ dailytasks/src/main.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Calander from "./components/Calander.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "calendar",
+        element: <Calander />,
+      },
+    ],
+    // TODO -- Create errorElement to handle bad urls
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
