b863d040cdd9631bb1aeb102aa5775abdf2f6dd6
Alex Singh <115970252+singhalex@users.noreply.github.com>
2024-02-16 12:47:07 -0800

    CSS Reset

--- dailytasks/src/App.jsx
+++ dailytasks/src/App.jsx
@@ -1,9 +1,11 @@
 import Sidebar from "./components/SideBar";
 import { Outlet } from "react-router-dom";
+import GlobalStyle from "./components/styles/globalStyles";
 
 const App = () => {
   return (
     <>
+      <GlobalStyle />
       <Sidebar />
       <Outlet />
     </>
