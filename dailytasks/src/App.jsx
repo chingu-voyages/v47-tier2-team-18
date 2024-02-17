6387155010b9c169fdecefb40761cda0459f11f4
Alex Singh <115970252+singhalex@users.noreply.github.com>
2024-02-16 16:06:19 -0800

    Assemble the app using grid

--- dailytasks/src/App.jsx
+++ dailytasks/src/App.jsx
@@ -1,13 +1,35 @@
 import Sidebar from "./components/SideBar";
 import { Outlet } from "react-router-dom";
 import GlobalStyle from "./components/styles/globalStyles";
+import {
+  AppWrapper,
+  ContentWrapper,
+  FooterWrapper,
+  HeaderWrapper,
+  NavigationWrapper,
+} from "./components/styles/App.styled";
 
 const App = () => {
   return (
     <>
+      {/* Apply global styles to all components in the app */}
       <GlobalStyle />
-      <Sidebar />
-      <Outlet />
+      <AppWrapper>
+        <HeaderWrapper>
+          {/* TODO -- replace with actual header component */}
+          <div>Temp Header</div>
+        </HeaderWrapper>
+        <NavigationWrapper>
+          <Sidebar />
+        </NavigationWrapper>
+        <ContentWrapper>
+          <Outlet />
+        </ContentWrapper>
+        {/* TODO -- replace with actual footer component */}
+        <FooterWrapper>
+          <div>Temp Footer</div>
+        </FooterWrapper>
+      </AppWrapper>
     </>
   );
 };
