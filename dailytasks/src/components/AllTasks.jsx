4d193e7c100e7735d9861f11ce51ae51c9ef3960
hectorgarcia07 <csharp12321@gmail.com>
2024-02-16 21:26:39 -0600

    updated calander and single view of tasks

--- dailytasks/src/components/AllTasks.jsx
+++ dailytasks/src/components/AllTasks.jsx
@@ -4,19 +4,26 @@ import { AllTasksWrapper } from './styles/AllTasksStyles';
 import { useLocation } from 'react-router-dom';
 
 const AllTasks = () => {
-  let { state: {tasks}  } = useLocation();
+  let {state} = useLocation();
+  console.log("SSS", state)
+  let tasks = state ? { state: {tasks}  } : undefined
+
   console.log("State", tasks)
 
   return (
     <>
-      {tasks.categories ?
+      {tasks ?
       <AllTasksWrapper>
         {tasks.categories.map((category, index) => (
           <Category key={index} category={category} />
         ))}
       </AllTasksWrapper>
       :
-      <p>No tasks to display</p>}
+      <AllTasksWrapper>
+        {tasksData.map((category, index) => (
+          <Category key={index} category={category} />
+        ))}
+      </AllTasksWrapper>}
   </>
   );
 };
