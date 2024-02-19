e8e097a6cef469cd081a77106bd0ac44fb81e62a
Alex Singh <115970252+singhalex@users.noreply.github.com>
2024-02-19 12:03:08 -0800

    Finish form to add Tasks

--- dailytasks/src/components/TaskForm/TaskForm.jsx
+++ dailytasks/src/components/TaskForm/TaskForm.jsx
@@ -12,6 +12,12 @@ const TaskForm = () => {
   const [taskName, setTaskName] = useState("");
   const [taskDescription, setTaskDescription] = useState("");
   const [taskDays, setTaskDays] = useState([]);
+  const [taskAdded, setTaskAdded] = useState(false);
+
+  // Invalid states
+  const [invalidTaskName, setInvalidTaskName] = useState(false);
+  const [invalidTaskDescription, setInvalidTaskDescription] = useState(false);
+  const [invalidTaskDays, setInvalidTaskDays] = useState(false);
 
   // Deals with the days portion of the form
   const [frequencyToggle, setFrequencyToggle] = useState(true);
@@ -19,6 +25,7 @@ const TaskForm = () => {
     setFrequencyToggle(event.target.value === "day-week" ? true : false);
     setTaskDays([]);
   };
+
   const addDays = (event) => {
     // Runs when box is checked
     if (event.target.checked) {
@@ -34,81 +41,114 @@ const TaskForm = () => {
     setTaskDays(taskDays.filter((day) => day !== event.target.value));
   };
 
-  // Need to implement
-  const checkValid = () => {
-    // Stuff in here
+  const isInvalid = () => {
+    if (taskName === "") {
+      setInvalidTaskName(true);
+    } else {
+      setInvalidTaskName(false);
+    }
+
+    if (taskDescription === "") {
+      setInvalidTaskDescription(true);
+    } else {
+      setInvalidTaskDescription(false);
+    }
+
+    if (taskDays.length < 1) {
+      setInvalidTaskDays(true);
+    } else {
+      setInvalidTaskDays(false);
+    }
+
+    if (taskName === "" || taskDescription === "" || taskDays.length < 1)
+      return true;
+
+    return false;
   };
+
   const addTask = (event) => {
     event.preventDefault();
 
-    //Check for empty inputs
-    if (taskName === "" || taskDescription === "" || taskDays.length < 1) {
-      return;
-    }
+    if (isInvalid()) return;
 
     setTaskArray([
       ...taskArray,
       { taskName: taskName, taskDescription: taskDescription, days: taskDays },
     ]);
 
-    navigate("/");
+    setTaskAdded(true);
+
+    setTimeout(() => {
+      navigate("/");
+    }, 2000);
   };
 
   return (
-    <form action="">
-      <div style={{ display: "flex", flexDirection: "column" }}>
-        <label htmlFor="task-name">Task Name</label>
-        <input
-          type="text"
-          name="task-name"
-          id="task-name"
-          value={taskName}
-          onChange={(event) => setTaskName(event.target.value)}
-          required
-        />
-      </div>
-
-      <div style={{ display: "flex", flexDirection: "column" }}>
-        <label htmlFor="description">Description</label>
-        <textarea
-          name="description"
-          id="description"
-          value={taskDescription}
-          onChange={(event) => setTaskDescription(event.target.value)}
-        />
-      </div>
-
-      <div>
-        <input
-          type="radio"
-          name="frequency"
-          id="day-week"
-          value="day-week"
-          onChange={handleFrequencyToggle}
-          checked={frequencyToggle}
-        />
-        <label htmlFor="day-week">Day of the Week</label>
-      </div>
-
-      <div>
-        <input
-          type="radio"
-          name="frequency"
-          id="day-month"
-          value="day-month"
-          onChange={handleFrequencyToggle}
-        />
-        <label htmlFor="day-month">Day of the Month</label>
-      </div>
-
-      {frequencyToggle ? (
-        <DaysWeekCheckBox onChange={addDays} />
+    <>
+      {taskAdded ? (
+        <div>Task Added</div>
       ) : (
-        <DaysMonthInput updateDaysArray={setTaskDays} />
-      )}
+        <form action="">
+          <div style={{ display: "flex", flexDirection: "column" }}>
+            <label htmlFor="task-name">Task Name</label>
+            {invalidTaskName && <div>Task name must not be empty</div>}
+            <input
+              type="text"
+              name="task-name"
+              id="task-name"
+              value={taskName}
+              onChange={(event) => setTaskName(event.target.value)}
+              required
+            />
+          </div>
 
-      <button onClick={addTask}>Add</button>
-    </form>
+          <div style={{ display: "flex", flexDirection: "column" }}>
+            <label htmlFor="description">Description</label>
+            {invalidTaskDescription && (
+              <div>Task description must not be empty</div>
+            )}
+            <textarea
+              name="description"
+              id="description"
+              value={taskDescription}
+              onChange={(event) => setTaskDescription(event.target.value)}
+            />
+          </div>
+
+          <div>
+            <input
+              type="radio"
+              name="frequency"
+              id="day-week"
+              value="day-week"
+              onChange={handleFrequencyToggle}
+              checked={frequencyToggle}
+            />
+            <label htmlFor="day-week">Day of the Week</label>
+          </div>
+
+          <div>
+            <input
+              type="radio"
+              name="frequency"
+              id="day-month"
+              value="day-month"
+              onChange={handleFrequencyToggle}
+            />
+            <label htmlFor="day-month">Day of the Month</label>
+          </div>
+
+          {invalidTaskDays && <div>Must select a day</div>}
+          {frequencyToggle ? (
+            <DaysWeekCheckBox onChange={addDays} />
+          ) : (
+            <DaysMonthInput updateDaysArray={setTaskDays} />
+          )}
+
+          <button onClick={addTask}>Add</button>
+        </form>
+      )}
+    </>
   );
 };
 export default TaskForm;
