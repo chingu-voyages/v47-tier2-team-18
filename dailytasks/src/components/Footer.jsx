978793a0fb06d550e5461fa35b093b20c7338bd7
Valeriusdev <kakao757@gmail.com>
2024-02-21 03:49:41 -0800

    add Footer component

---
+++ dailytasks/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import { StyledFooter, FooterLink } from "./styles/FooterStyles";
+
+const Footer = () => {
+  return (
+    <StyledFooter>
+      <FooterLink href="https://github.com/Valeriusdev"> Valeriy Lysenko </FooterLink>
+      <FooterLink href="https://github.com/singhalex"> Alex Singh </FooterLink>
+      <FooterLink href="https://github.com/hectorgarcia07"> Hector Garcia </FooterLink>
+    </StyledFooter>
+  );
+};
+
+export default Footer;
--- dailytasks/src/components/styles/AllTasksStyles.jsx
+++ dailytasks/src/components/styles/AllTasksStyles.jsx
@@ -4,7 +4,7 @@ export const AllTasksWrapper = styled.div`
   border: 4px solid #7029b7;
   border-radius: 10px;
   font-family: 'Open Sans', sans-serif;
-  max-width: 650px;
+  max-width: 80vh;
   margin: 0 auto;
   padding: 25px;
   background-color: #cfffb8;
